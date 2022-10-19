e6f6f4b93f92cd20340e78442a7cd66050824fcf
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2022-10-19 20:32:32 +0900

    ADD : Local 로직 코드 추가

---
+++ data/src/main/java/kr/hs/dgsw/hyeon/data/local/entity/CenterEntity.kt
@@ -0,0 +1,16 @@
+package kr.hs.dgsw.hyeon.data.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "center_table")
+data class CenterEntity(
+    @PrimaryKey
+    @ColumnInfo(name = "id") val id: Int,
+    @ColumnInfo(name = "address") val address: String,
+    @ColumnInfo(name = "center_name") val centerName: String,
+    @ColumnInfo(name = "facility_name") val facilityName: String,
+    @ColumnInfo(name = "phone_number") val phone_number: String,
+    @ColumnInfo(name = "updated_at") val updatedAt: String
+)
