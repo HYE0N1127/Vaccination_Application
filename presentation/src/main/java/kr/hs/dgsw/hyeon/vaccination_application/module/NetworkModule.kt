54296115ac86eb978db35cdac96846077bd832d1
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2022-10-20 11:51:00 +0900

    ADD : NetworkModule

---
+++ presentation/src/main/java/kr/hs/dgsw/hyeon/vaccination_application/module/NetworkModule.kt
@@ -0,0 +1,33 @@
+package kr.hs.dgsw.hyeon.vaccination_application.module
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kr.hs.dgsw.hyeon.vaccination_application.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient().newBuilder()
+            .addNetworkInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BuildConfig.BASE_URL)
+            .build()
+}
