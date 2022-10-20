f99c35befcb023f70b3644bdd7a4608c91cb10ce
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2022-10-20 11:50:36 +0900

    Fix : BuildConfig 를 이용한 상수 관리 방식으로 변경

--- data/src/main/java/kr/hs/dgsw/hyeon/data/service/CenterService.kt
+++ data/src/main/java/kr/hs/dgsw/hyeon/data/service/CenterService.kt
@@ -1,13 +1,15 @@
 package kr.hs.dgsw.hyeon.data.service
 
-import kr.hs.dgsw.hyeon.data.model.Response
+import kr.hs.dgsw.hyeon.data.BuildConfig
 import kr.hs.dgsw.hyeon.data.model.CenterResponse
+import kr.hs.dgsw.hyeon.data.model.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface CenterService {
     @GET("15077586/v1/centers")
     suspend fun getCenterDataByRemote(
-        @Query("page") page: Int
+        @Query("page") page: Int,
+        @Query("serviceKey") serviceKey: String = BuildConfig.ENCODE_API_KEY
     ): Response<List<CenterResponse>>
 }
---
+++ presentation/src/main/java/kr/hs/dgsw/hyeon/vaccination_application/VaccinationApplication.kt
@@ -0,0 +1,8 @@
+package kr.hs.dgsw.hyeon.vaccination_application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class VaccinationApplication: Application() {
+}
